874853c4d3986c82ff445b97c69d061dd6403c27
Razzka Rusmantoro <razzkarusmantoro123@gmail.com>
2024-06-07 23:38:16 +0400

    chat box update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,23 @@
 // components/Chat.js
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../App.css";
 
 const Chat = () => {
   const [message, setMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const chatBoxRef = useRef(null); // created a reference for chat box
 
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
   };
+
+
+  // implemented useEffect hook to scroll to bottom of the chat when a new message enters the chat
+  useEffect(() => {
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+    }
+  }, [messageList]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -18,14 +27,18 @@ const Chat = () => {
 
   return (
     <div className="chat-container">
-      <div className="chat-box">
+      <div className="chat-box" ref={chatBoxRef}>
         <div className="message-list">
-        {messageList.map((msg, index) => (
+          {messageList.map((msg, index) => (
             <div key={index} className="message">
-                <img className="sender-icon" src="https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kamisato_ayaka/image.png?strip=all&quality=100"></img> 
-                <div>
-                <div className="sender-name">Ayaka</div> 
-              <div className="message-text">{msg}</div>
+              <img
+                className="sender-icon"
+                src="https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/characters/kamisato_ayaka/image.png?strip=all&quality=100"
+                alt="sender-icon"
+              />
+              <div>
+                <div className="sender-name">Ayaka</div>
+                <div className="message-text">{msg}</div>
               </div>
             </div>
           ))}
@@ -37,8 +50,9 @@ const Chat = () => {
           value={message}
           onChange={handleMessageChange}
           placeholder="Type your message..."
+
         />
-       
+
       </form>
     </div>
   );
