65906a5c6590a17e59638c5e9e81057ddd7c390d
Razzka Rusmantoro <razzkarusmantoro123@gmail.com>
2024-06-08 10:53:33 +0400

    payment page UI updated

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -10,30 +10,32 @@ const Payment = () => {
     <div className="payment-container">
       <div className="payment-content">
         <h2 className="text">Payment</h2>
-        <p>
-          <input type="radio" id="creditCard" name="paymentOption" value="creditCard" />
-          <label htmlFor="creditCard" style={{ fontSize: "20px" }}>
-            Credit Cards
-          </label>
-        </p>
-        <p>
-          <input type="radio" id="paypal" name="paymentOption" value="paypal" />
-          <label htmlFor="paypal">
-            <img src={paypalImg} alt="PayPal" style={{ width: "60px" }} />
-          </label>
-        </p>
-        <p>
-          <input type="radio" id="ethereum" name="paymentOption" value="ethereum" />
-          <label htmlFor="ethereum">
-            <img src={ethImg} alt="Ethereum" style={{ width: "17.5px" }} />
-          </label>
-        </p>
-        <p>
-          <input type="radio" id="bitcoin" name="paymentOption" value="bitcoin" />
-          <label htmlFor="bitcoin">
-            <img src={btcImg} alt="Bitcoin" style={{ width: "35px" }} />
-          </label>
-        </p>
+        <div className="radioButtonsDiv">
+          <div className="radioOption">
+            <input type="radio" id="creditCard" name="paymentOption" value="creditCard" />
+            <label htmlFor="creditCard" style={{ fontSize: "15px", marginLeft: "0px",}}>
+                Credit Cards
+            </label>
+          </div>
+          <div className="radioOption">
+            <input type="radio" id="paypal" name="paymentOption" value="paypal" />
+            <label htmlFor="paypal">
+              <img src={paypalImg} className = "mleft"alt="PayPal" style={{ width: "60px" }} />
+            </label>
+          </div>
+          <div className="radioOption">
+            <input type="radio" id="ethereum" name="paymentOption" value="ethereum" />
+            <label htmlFor="ethereum">
+              <img src={ethImg} className = "mleft" alt="Ethereum" style={{ width: "17.5px" }} />
+            </label>
+          </div>
+          <div className="radioOption">
+            <input type="radio" id="bitcoin" name="paymentOption" value="bitcoin" />
+            <label htmlFor="bitcoin">
+              <img src={btcImg} alt="Bitcoin" style={{ width: "35px" }} />
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   );
