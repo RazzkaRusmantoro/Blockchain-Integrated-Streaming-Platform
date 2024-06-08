abb279b117fafac67eb60485eff0f31e59dd1a3e
Clerissa <clerissa@rolzo.com>
2024-06-08 10:24:12 +0400

    homeui fixed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <>
       {isHome && <Navbar />}
-      <div className={`${isHome ? "with-navbar content" : ""}`}>
+      <div className={`${isHome ? "content" : ""}`}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/watch-party" element={<WatchParty/>} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,45 @@
 import React from "react";
-import Countdown from 'react-countdown';
+import Countdown from "react-countdown";
 import homeImage from "../assets/homepage.jpg";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const targetTime = Date.now() + 1 * 60 * 60 * 1000 + 47 * 60 * 1000 + 12 * 1000;
+  const navigate = useNavigate();
+
+  const targetTime =
+    Date.now() + 1 * 60 * 60 * 1000 + 47 * 60 * 1000 + 12 * 1000;
 
   const renderer = ({ hours, minutes, seconds }) => (
-    <span className="countdown">{hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</span>
+    <span className="countdown">
+      {hours.toString().padStart(2, "0")}:{minutes.toString().padStart(2, "0")}:
+      {seconds.toString().padStart(2, "0")}
+    </span>
   );
 
   return (
-    <div className="home-container">
-      <img src={homeImage} alt="Homepage" className="home-img" />
-      <div className="home-text">
-        <div class="text-container">
-          {/* <p className="text-bottom">VALORANT</p> */}
-          <p className="text-top">NOW LIVE</p>
-          <Countdown date={targetTime} renderer={renderer} />
+    <>
+      <div className="home-container">
+        <img src={homeImage} alt="Homepage" className="home-img" />
+        <div className="overlay"></div>
+        <div className="home-text">
+          <div className="text-container">
+            <p className="text-top">NOW LIVE</p>
+            <Countdown date={targetTime} renderer={renderer} />
+          </div>
+          <button
+            onClick={() => {
+              navigate("/watch-party");
+            }}
+            className="start-connection"
+          >
+            Start Connection
+          </button>
         </div>
-        <button className="start-connection">Start Connection</button>
       </div>
-    </div>
+      <div className="events-container">
+        <p className="text-events">CONNECT WITH OTHER EVENTS</p>
+      </div>
+    </>
   );
 };
 
